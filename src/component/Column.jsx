84848eabbbfee4fc089ab7425d0014d504c6268c
s20016 <s20016@std.it-college.ac.jp>
2023-12-16 22:27:56 +0900

    Add: Update, delete function

--- src/component/Column.jsx
+++ src/component/Column.jsx
@@ -19,39 +19,52 @@ export default function Column({ func, data }) {
     todo: 'To-Do',
     prog: 'In Progress',
     done: 'Completed',
-  }[data.name];
+  };
 
   const columnSubtitle = {
     todo: 'バックログ',
     prog: '進行中',
     done: '完了',
-  }[data.name];
+  };
 
   /* ---------------------------------- Modal --------------------------------- */
-  const [modalIsOpen, setIsOpen] = useState(false);
+  const [addModal, setAddModal] = useState(false);
+  const [editModal, setEditModal] = useState(false);
+  const [activeEditModal, setActiveEditModal] = useState({
+    item: data.defaultData,
+    itemIndex: 0,
+  });
 
-  function openModal() {
-    setIsOpen(true);
-  }
+  const openAddModal = () => {
+    setAddModal(true);
+  };
 
-  function closeModal() {
-    setIsOpen(false);
-  }
+  const closeAddModal = () => {
+    setAddModal(false);
+  };
+
+  const openEditModal = () => {
+    setEditModal(true);
+  };
+
+  const closeEditModal = () => {
+    setEditModal(false);
+  };
 
   const addItemModal = (name) => {
     return (
       <ReactModal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
+        isOpen={addModal}
+        onRequestClose={closeAddModal}
         style={customStyles}
         className="add-modal"
-        contentLabel="Output Modal"
+        contentLabel="Add Modal"
       >
         <div className="add-modal__wrapper">
           <header className="add-modal__header">
-            <h2 className="add-modal__title">Add {columnTitle}</h2>
+            <h2 className="add-modal__title">Add {columnTitle[data.name]}</h2>
             <small className="add-modal__subtitle">
-              {columnSubtitle}を追加する
+              {columnSubtitle[data.name]}を追加する
             </small>
           </header>
           <form
@@ -59,7 +72,7 @@ export default function Column({ func, data }) {
             id="add-item-form"
             onSubmit={(e) => {
               func.handleAddItem(e, name);
-              closeModal();
+              closeAddModal();
             }}
           >
             <div className="form__item">
@@ -68,7 +81,9 @@ export default function Column({ func, data }) {
                 type="text"
                 name="title"
                 id="title"
-                defaultValue={`${columnSubtitle}タスク #${columnCount + 1}`}
+                defaultValue={`${columnSubtitle[data.name]}タスク #${
+                  columnCount + 1
+                }`}
               />
             </div>
             <div className="form__item">
@@ -78,6 +93,7 @@ export default function Column({ func, data }) {
                 id="description"
                 cols="30"
                 rows="3"
+                placeholder={`タスク内容を入力してください`}
               ></textarea>
             </div>
             <div className="form__item">
@@ -88,9 +104,107 @@ export default function Column({ func, data }) {
               </small>
             </div>
             <div className="form__buttons">
-              <button type="button" onClick={closeModal}>
+              <button type="button" onClick={closeAddModal}>
                 キャンセル
               </button>
+              <button type="submit">追加する</button>
+            </div>
+          </form>
+        </div>
+      </ReactModal>
+    );
+  };
+
+  const editItemModal = ({ item, itemIndex }) => {
+    const itemId = item.id;
+    const itemTitle = item.title;
+    const itemDecription = item.description;
+    const itemTag = item.tag ? item.tag.join(' ') : '';
+    const datetime = {
+      todo: [item.date.todo || '--'],
+      prog: [item.date.prog || '--'],
+      done: [item.date.done || '--'],
+    };
+
+    const itemDate = (
+      <table className="table">
+        <tbody className="table__body">
+          {Object.entries(datetime).map((item, timeIndex) => {
+            return (
+              <tr className="table__row" key={timeIndex}>
+                <th className="table__title">{columnTitle[item[0]]}</th>
+                <td className="table__text">{item[1]}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+
+    return (
+      <ReactModal
+        isOpen={editModal}
+        onRequestClose={closeEditModal}
+        style={customStyles}
+        className="add-modal"
+        contentLabel="Edit Modal"
+      >
+        <div className="add-modal__wrapper">
+          <header className="add-modal__header">
+            <h2 className="add-modal__title">Edit {itemTitle}</h2>
+            <small className="add-modal__subtitle">タスク内容を変更する</small>
+          </header>
+          {itemDate}
+          <form
+            className="form"
+            id="add-item-form"
+            onSubmit={(e) => {
+              func.handleEditItem(e, data.name, item, itemIndex);
+              closeEditModal();
+            }}
+          >
+            <div className="form__item">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                name="title"
+                id="title"
+                defaultValue={itemTitle}
+              />
+            </div>
+            <div className="form__item">
+              <label htmlFor="description">Description</label>
+              <textarea
+                name="description"
+                id="description"
+                cols="30"
+                rows="3"
+                placeholder={`タスク内容を入力してください`}
+                defaultValue={itemDecription}
+              ></textarea>
+            </div>
+            <div className="form__item">
+              <label htmlFor="tags">Tags</label>
+              <input type="text" name="tags" id="tags" defaultValue={itemTag} />
+              <small className="form__note">
+                ※タグの間には半角スペースを入力してください
+              </small>
+            </div>
+            <div className="form__buttons form__buttons--edit">
+              <div className="form__button-box">
+                <button type="button" onClick={closeEditModal}>
+                  キャンセル
+                </button>
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    func.handleDeleteItem(e, data.name, itemIndex);
+                    closeEditModal();
+                  }}
+                >
+                  削除する
+                </button>
+              </div>
               <button type="submit">追加する</button>
             </div>
           </form>
@@ -102,25 +216,32 @@ export default function Column({ func, data }) {
   /* ---------------------------------- Items --------------------------------- */
 
   const addItem = (
-    <button className="column__add" onClick={openModal}>
+    <button className="column__add" onClick={openAddModal}>
       <span className="material-symbols-outlined">add</span>
     </button>
   );
 
   if (userList.length) {
     const items = userList.map((item, index) => {
-      const tags = item.tag.map((tag, index) => {
+      const tags = item.tag.map((tag, tagIndex) => {
         return (
-          <li className="card__tags" key={index}>
+          <li className="card__tags" key={`${data.name}-tag-${tagIndex}`}>
             <small className="card__tag">#{tag}</small>
           </li>
         );
       });
 
       return (
-        <li className="card" key={index}>
+        <li
+          className="card"
+          key={item.id}
+          onClick={() => {
+            setActiveEditModal({ item: item, itemIndex: index });
+            openEditModal();
+          }}
+        >
           <header className="card__header">
-            <small className="card__date">{item.date}</small>
+            <small className="card__date">{item.date[data.name]}</small>
             <h3 className="card__title">{item.title}</h3>
           </header>
           <ul className="card__list">{tags}</ul>
@@ -139,10 +260,10 @@ export default function Column({ func, data }) {
       {addItemModal(data.name)}
       <div className="column-top">
         <header className="column-top__header">
-          <h2 className="column-top__title">{columnTitle}</h2>
+          <h2 className="column-top__title">{columnTitle[data.name]}</h2>
           <small className="column-top__subtitle">
             {/* {columnCount}コ・{columnSubtitle}リスト */}
-            {columnSubtitle}リスト
+            {columnSubtitle[data.name]}リスト
           </small>
         </header>
         {addItem}
@@ -151,6 +272,7 @@ export default function Column({ func, data }) {
         </div> */}
       </div>
       <ul className="column__list">{columnList}</ul>
+      {editModal ? editItemModal(activeEditModal) : null}
     </div>
   );
 }
--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -8,28 +8,52 @@ import Column from './Column';
 export default function Main() {
   const [userList, setUserList] = useState([]);
 
+  const defaultDate = {
+    todo: '',
+    prog: '',
+    done: '',
+  };
+
+  const defaultData = {
+    id: '',
+    title: '',
+    description: '',
+    tag: [],
+    date: {
+      ...defaultDate,
+    },
+  };
+
   useEffect(() => {
     const localList = localStorage.getItem('kanbanArrays');
     loadSetting(localList, settings);
   }, []);
 
-  const validate = (item) => {
+  const newDate = () => {
     const now = moment().locale('ja');
-    const date = now.format('MMMDo H:mm');
+    return now.format('MMMDo H:mm');
+  };
+
+  const validate = (item, name) => {
+    const date = newDate();
 
     return {
-      id: crypto.randomUUID(),
-      date: date,
       ...item,
+      id: crypto.randomUUID(),
+      date: {
+        ...defaultDate,
+        ...item.date,
+        [name]: date,
+      },
     };
   };
 
   const loadSetting = (list, setting) => {
     if (!list) {
       const updatedList = {
-        todo: setting.initialList.todo.map((item) => validate(item)),
-        prog: setting.initialList.prog.map((item) => validate(item)),
-        done: setting.initialList.done.map((item) => validate(item)),
+        todo: setting.initialList.todo.map((item) => validate(item, 'todo')),
+        prog: setting.initialList.prog.map((item) => validate(item, 'prog')),
+        done: setting.initialList.done.map((item) => validate(item, 'done')),
       };
 
       localStorage.setItem('kanbanArrays', JSON.stringify(updatedList));
@@ -40,7 +64,7 @@ export default function Main() {
     setUserList(localList);
   };
 
-  /* ------------------------------- Update List ------------------------------ */
+  /* ----------------------------- Update Storage ----------------------------- */
   const handleUpdateList = (list) => {
     console.log('Update: List');
     setUserList(list);
@@ -57,11 +81,17 @@ export default function Main() {
       ? tags.value.split(' ').filter((tag) => tag)
       : [];
 
-    const item = validate({
-      title: title.value,
-      description: description.value,
-      tag: tagList,
-    });
+    const item = validate(
+      {
+        title: title.value,
+        description: description.value,
+        tag: tagList,
+        date: {
+          ...defaultDate,
+        },
+      },
+      name
+    );
 
     const newItem = {
       ...userList,
@@ -71,27 +101,73 @@ export default function Main() {
     handleUpdateList(newItem);
   };
 
+  const handleEditItem = (event, name, newArray, index) => {
+    event.preventDefault();
+
+    const { title, description, tags } = event.target;
+    const tagList = tags.value
+      ? tags.value.split(' ').filter((tag) => tag)
+      : [];
+
+    const item = {
+      ...newArray,
+      title: title.value,
+      description: description.value,
+      tag: tagList,
+      date: {
+        ...newArray.date,
+        [name]: newDate(),
+      },
+    };
+
+    const newItem = {
+      ...userList,
+      [name]: [
+        ...userList[name].slice(0, index),
+        item,
+        ...userList[name].slice(index + 1),
+      ],
+    };
+
+    handleUpdateList(newItem);
+  };
+
+  const handleDeleteItem = (event, name, index) => {
+    event.preventDefault();
+
+    const newItem = {
+      ...userList,
+      [name]: [
+        ...userList[name].slice(0, index),
+        ...userList[name].slice(index + 1),
+      ],
+    };
+
+    handleUpdateList(newItem);
+  };
+
   /* -------------------------------------------------------------------------- */
 
   const columnFunctions = {
     handleAddItem: handleAddItem,
+    handleEditItem: handleEditItem,
+    handleDeleteItem: handleDeleteItem,
+  };
+
+  const columnData = (name) => {
+    return {
+      name: name,
+      userList: userList[name],
+      defaultData: defaultData,
+    };
   };
 
   return (
     <main className="main">
       <section className="main__wrapper">
-        <Column
-          func={columnFunctions}
-          data={{ name: 'todo', userList: userList.todo }}
-        />
-        <Column
-          func={columnFunctions}
-          data={{ name: 'prog', userList: userList.prog }}
-        />
-        <Column
-          func={columnFunctions}
-          data={{ name: 'done', userList: userList.done }}
-        />
+        <Column func={columnFunctions} data={columnData('todo')} />
+        <Column func={columnFunctions} data={columnData('prog')} />
+        <Column func={columnFunctions} data={columnData('done')} />
       </section>
     </main>
   );
